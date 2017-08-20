ab8a353b07c4c93bd650f5ada57addd94886ad1f
nilvisa <lo_nilsson@hotmail.com>
2017-08-20 15:35:46 +0200

    4-42

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combinedReducers } from 'redux';
+import BooksReducer from './reducer_books';
+
+const rootReducer = combinedReducers({
+  books: BooksReducer
+});
+
+export default rootReducer;
---
+++ src/reducers/reducers__books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript - the goodish' },
+    { title: 'Harry Potter' },
+    { title: 'The dark tower' },
+    { title: 'Johans flygbok' }
+  ]
+}
