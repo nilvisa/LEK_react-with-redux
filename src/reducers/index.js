ddd0d8910937dcfbb277c11abac29a20718bedec
Lovisa Junemo <lovjun@lbi.local>
2017-08-21 17:19:06 +0200

    4-50

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducers_books.js';
+import ActiveBook from './reducers_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
--- src/reducers/reducers_books.js
+++ src/reducers/reducers_books.js
@@ -1,8 +1,20 @@
 export default function () {
   return [
-    { title: 'Javascript - the goodish' },
-    { title: 'Harry Potter' },
-    { title: 'The dark tower' },
-    { title: 'Johans flygbok' }
+    {
+      title: 'Javascript - the goodish',
+      pages: 759
+    },
+    {
+      title: 'Harry Potter',
+      pages: 8363
+    },
+    {
+      title: 'The dark tower',
+      pages: 7545
+    },
+    {
+      title: 'Johans flygbok',
+      pages: 10
+    }
   ]
 }
